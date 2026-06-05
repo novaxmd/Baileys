c78f6c47581b58bb26d26eba05b9376b9071f2ec
xh_clinton <152104512+xhclintohn@users.noreply.github.com>
2026-06-05 04:53:22 +0300

    Update Newsletter.ts

--- src/Types/Newsletter.ts
+++ src/Types/Newsletter.ts
@@ -12,20 +12,6 @@ export enum XWAPaths {
 	xwa2_newsletter_demote = 'xwa2_newsletter_demote',
 	xwa2_newsletter_delete_v2 = 'xwa2_newsletter_delete_v2'
 }
-export enum QueryIds {
-	CREATE = '8823471724422422',
-	UPDATE_METADATA = '24250201037901610',
-	METADATA = '6563316087068696',
-	SUBSCRIBERS = '9783111038412085',
-	FOLLOW = '7871414976211147',
-	UNFOLLOW = '7238632346214362',
-	MUTE = '29766401636284406',
-	UNMUTE = '9864994326891137',
-	ADMIN_COUNT = '7130823597031706',
-	CHANGE_OWNER = '7341777602580933',
-	DEMOTE = '6551828931592903',
-	DELETE = '30062808666639665'
-}
 export type NewsletterUpdate = {
 	name?: string
 	description?: string
